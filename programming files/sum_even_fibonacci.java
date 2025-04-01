434fabd4163e09c821b24bf3c751d6c9bdae2174
donovanj44 <56608638+donovanj44@users.noreply.github.com>
2025-04-01 16:58:47 -0400

    Update sum_even_fibonacci.java

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -1,13 +1,16 @@
+// JD
+// Reverse equality operator. Line 10
+// Remove line 14. b = a + b;
+
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
-            if (b % 2 == 0) {
+            if (b % 2 != 0) {
                 sumEven += b;
             }
             int temp = b;
-            b = a + b;
             a = temp;
         }
         System.out.println(sumEven);
