4012a944ad6af128d015e0afebc4a7c84cf62de6
keirahanson <112536877+keirahanson@users.noreply.github.com>
2025-04-01 16:44:29 -0400

    Update even_odd_site.java

--- programming files/even_odd_site.java
+++ programming files/even_odd_site.java
@@ -7,21 +7,21 @@ public class even_odd_site {
         BufferedWriter file = new BufferedWriter(new FileWriter("numbers.html"));
         file.write("<html>\n<head>\n<title>List of Numbers</title>\n</head>\n<body>\n");
         file.write("<table>\n<tr><th>Even Numbers</th><th>Odd Numbers</th></tr>\n");
-        for (int i = 1; i <= 50; i++) {
-            if (i % 2 == 0) {
-                file.write("<tr><td>" + i + "</td><td></td></tr>\n");
-            }
-            else {
-                file.write("<tr><td></td><td>" + i + "</td></tr>\n");
-            }
+        // for (int i = 1; i <= 50; i++) {
+        //     if (i % 2 == 0) {
+        //         file.write("<tr><td>" + i + "</td><td></td></tr>\n");
+        //     }
+        //     else {
+        //         file.write("<tr><td></td><td>" + i + "</td></tr>\n");
+        //     }
         }
         file.write("</table>\n</body>\n</html>");
         file.close();
-        BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            System.out.println(line);
-        }
+        // BufferedReader reader = new BufferedReader(new FileReader("numbers.html"));
+        // String line;
+        // while ((line = reader.readLine()) != null) {
+        //     System.out.println(line);
+        // }
         reader.close();
     }
 }
