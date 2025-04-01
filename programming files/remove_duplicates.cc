da9cf342c5cf865c30f1149f342ab083ad7e81d1
keirahanson <112536877+keirahanson@users.noreply.github.com>
2025-04-01 17:00:37 -0400

    bugs added remove_duplicates.cc

--- programming files/remove_duplicates.cc
+++ programming files/remove_duplicates.cc
@@ -9,9 +9,10 @@ vector<int> remove_duplicates(vector<int> arr, vector<int>& removed_elements){
     vector<int> unique_arr;
     sort(arr.begin(), arr.end());
     int n = arr.size();
+    // bugs added
     for(int i=0; i<n; i++){
-        if(i == 0 || arr[i] != arr[i-1]){
-            unique_arr.push_back(arr[i]);
+        if(i == 0 || arr[i] != arr[i]){
+            unique_arr.push_back(arr[0]);
         }
         else{
             removed_elements.push_back(arr[i]);
