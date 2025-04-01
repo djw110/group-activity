7f152b21b06e30952b23471f2a3d111afe0b6ef5
Jake Potter <122999198+TheBuninnator@users.noreply.github.com>
2025-04-01 17:20:19 -0400

    Fixed bugs datetime_convert.java
    added a space between the data and time on line 6, capitalized MM and HH on line 7, switched formatted and dateStr

--- programming files/datetime_convert.java
+++ programming files/datetime_convert.java
@@ -3,9 +3,9 @@ import java.time.format.DateTimeFormatter;
 
 public class datetime_convert {
     public static void main(String[] args) {
-        String dateStr = "2022-03-1710:45:30";
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-mm-dd hh:mm:ss");
-        LocalDateTime dateObj = LocalDateTime.parse(formatter, dateStr);
+        String dateStr = "2022-03-17 10:45:30";
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        LocalDateTime dateObj = LocalDateTime.parse(dateStr, formatter);
         String formattedDate = dateObj.format(DateTimeFormatter.ofPattern("MM/dd/yyyy HH:mm:ss"));
 
         System.out.println(formattedDate);
