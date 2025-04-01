fbb3ba63c39a1726a77ac7f651894e2b49244e04
EvanStowell <150971025+EvanStowell@users.noreply.github.com>
2025-04-01 17:24:49 -0400

    Fixed bug sum_even_fibonacci.java
    Changed != to == on line 13
    Added b = a + b; after line 16

--- programming files/sum_even_fibonacci.java
+++ programming files/sum_even_fibonacci.java
@@ -2,15 +2,19 @@
 // Reverse equality operator. Line 10
 // Remove line 14. b = a + b;
 
+// ES
+// Bugs addressed
+
 public class sum_even_fibonacci {
     public static void main(String[] args) {
         int a = 0, b = 1;
         int sumEven = 0;
         while (b < 4000000) {
-            if (b % 2 != 0) {
+            if (b % 2 == 0) {
                 sumEven += b;
             }
             int temp = b;
+            b = a + b;
             a = temp;
         }
         System.out.println(sumEven);
