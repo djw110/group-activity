c055e4c0642be667a9acda39ebcfe136ed6d41cd
Joe <jdonova9@uvm.edu>
2025-04-01 17:19:12 -0400

    Issue #21

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -4,12 +4,12 @@
 
 # initial fibinocci numbers
 a, b = 0, 1
-sum_even = 1
+sum_even = 0
 # find fibonacci numbers up to 4,000,000
 
 while b < 4000000:
-    #if the fibonocci number 'b' is even
-    if b % 1 == 0:
+    # if the fibonocci number 'b' is even
+    if b % 2 == 0:
         # add it to a running sum
         sum_even += b
     # calculate the next fibonacci number in the series
