e5c96f65a4acbda3cc8882b0809290b1a2f98403
donovanj44 <56608638+donovanj44@users.noreply.github.com>
2025-04-01 17:01:13 -0400

    Update sum_even_fibonacci.py

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,11 +1,15 @@
+# JD
+# Change even or odd check to always be true. Line 12
+# Change initial value of sum_even to 1. Line 7
+
 # initial fibinocci numbers
 a, b = 0, 1
-sum_even = 0
+sum_even = 1
 # find fibonacci numbers up to 4,000,000
 
 while b < 4000000:
     #if the fibonocci number 'b' is even
-    if b % 2 == 0:
+    if b % 1 == 0:
         # add it to a running sum
         sum_even += b
     # calculate the next fibonacci number in the series
