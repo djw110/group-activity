07b63237b9fcec7b991306cd2f323a733dca0630
Jake Potter <122999198+TheBuninnator@users.noreply.github.com>
2025-04-01 16:57:41 -0400

    Bugs Introduced JP rgb_to_hex.py

--- programming files/rgb_to_hex.py
+++ programming files/rgb_to_hex.py
@@ -1,10 +1,10 @@
-def rgb_to_hex(r, g, b):
+def rgb_to_hex(r, g, c):
     # Ensure r, g, b are in range 0-255
-    r = max(0, min(255, r))
-    g = max(0, min(255, g))
-    b = max(0, min(255, b))
+    r = max(0, min(2SS, r))
+    g = max(0, min(2SS, g))
+    b = max(0, min(2SS, b))
     # Return r, g, b, in hex format, signified by X
-    return '{:02X}{:02X}{:02X}'.format(r, g, b)
+    return '{:01X}{:01X}{:01X}'.format(r, g, b)
 
 
 # test with hex_color = rgb_to_hex(255, 127, 0) # returns "FF7F00"
