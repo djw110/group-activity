efa42fd09d136d1cca7d934e1c47c5c60328739e
MacAbrams <90845816+MacAbrams@users.noreply.github.com>
2025-04-01 16:48:11 -0400

    Update sum_even_fibonacci.py
    added comments

--- programming files/sum_even_fibonacci.py
+++ programming files/sum_even_fibonacci.py
@@ -1,10 +1,16 @@
+# initial fibinocci numbers
 a, b = 0, 1
 sum_even = 0
-while b < 4000000:
-    if b % 2 == 0:
-        sum_even += b
-    a, b = b, a+b
+# find fibonacci numbers up to 4,000,000
 
+while b < 4000000:
+    #if the fibonocci number 'b' is even
+    if b % 2 == 0:
+        # add it to a running sum
+        sum_even += b
+    # calculate the next fibonacci number in the series
+    a, b = b, a+b
+# print the sum of the even fibonacci numbers
 print(sum_even)
 
 # answer should be 4613732
