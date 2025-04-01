3f6c48286c6abaf447392c022b638275e1721976
donovanj44 <56608638+donovanj44@users.noreply.github.com>
2025-04-01 16:48:47 -0400

    Update rgb_to_hex.java

--- programming files/rgb_to_hex.java
+++ programming files/rgb_to_hex.java
@@ -8,9 +8,11 @@ public class rgb_to_hex {
     }
 
     public static String rgbToHex(int r, int g, int b) {
+        // Ensure r, g, b are in range 0-255
         r = Math.min(255, Math.max(0, r));
         g = Math.min(255, Math.max(0, g));
         b = Math.min(255, Math.max(0, b));
+        // Return r, g, b in hex format, %02X represents each variable
         return String.format("%02X%02X%02X", r, g, b);
     }
 }
